b53401bc5af54d1af228019b9cc88a2c8a7ed398
k2supra <andrijkorolevic@gmail.com>
2022-12-20 20:18:19 +0200

    Created first code

---
+++ Project1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+//int main()//
+//{
+//	cout << "Name \t\t: ''The war and the peace'' \nAvtor \t\t: L.N.Tolstoj \nIzdatelstvo \t: Piter \nPages \t\t: 500" << endl;
+//	return 0;
+//}
+
+int main()
+{
+		cout << "Krai lukomor`a dub zelenuy, " << endl, Beep(247, 500), Beep(417, 500), Beep(417, 500),
+		cout << "\nIzolotyj lancyuh na nim:" << endl, Beep(370, 500), Beep(417, 500), Beep(329, 500),
+		cout << "\nShodnya, shonochi kit uchenyj " << endl, Beep(247, 500), Beep(247, 500), Beep(247, 500),
+		cout << "\nNa lancyuhu kruzhl`aje tim" << endl;
+	return 0;
+}
