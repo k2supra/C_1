31f297ec7127ec930be86be65b390a436a4f4b61
k2supra <andrijkorolevic@gmail.com>
2023-01-09 10:16:43 +0200

    done

---
+++ Project1/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "\n" << (char)201 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 <<
+		(char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 <<
+		(char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 <<
+		(char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 <<
+		(char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 <<(char)187 << 
+		"\n" << (char)186 << "\t\t\t\t\t       " << (char)186 <<
+		"\n" << (char)186 << "\t\t  Pory roku" << "\t\t       " << (char)186 <<
+		"\n" << (char)186 << "\t\t\t\t\t       " << (char)186 <<
+		"\n" << (char)204 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 <<
+		(char)205 << (char)205 << (char)203 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 <<
+		(char)205 << (char)205 << (char)205 << (char)205 << (char)203 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 <<
+		(char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)203 << (char)205 << (char)205 << (char)205 <<
+		(char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)185 <<
+		"\n" << (char)186 << "\t   " << (char)186 << "\t       " << (char)186 << "\t   " << (char)186 << "\t       " << (char)186 <<
+		"\n" << (char)186 << "   Zyma   " << (char)186 << "   Vesna   " << (char)186 << "   Lito    " << (char)186 << "   Osin    " << (char)186 <<
+		"\n" << (char)186 << "\t   " << (char)186 << "\t       " << (char)186 << "\t   " << (char)186 << "\t       " << (char)186 <<
+		"\n" << (char)200 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 <<
+		(char)205 << (char)205 << (char)202 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 <<
+		(char)205 << (char)205 << (char)205 << (char)205 << (char)202 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 <<
+		(char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)202 << (char)205 << (char)205 << (char)205 <<
+		(char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)188;
+}
